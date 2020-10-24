0e946321548dde06838e10d9676efd0419b689ad
Trangle91 <tlee.bon@gmail.com>
2020-10-24 15:11:07 -0500

    Updates

---
+++ precache-manifest.ddb1ac3d9f9474b598079e045071b18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947edd4e36421151104830ed906913a7",
+    "url": "/food-app/index.html"
+  },
+  {
+    "revision": "c8e82a6b46a8c36adeb9",
+    "url": "/food-app/static/css/main.a5ada5dc.chunk.css"
+  },
+  {
+    "revision": "698638a5f4ecb4fd1da5",
+    "url": "/food-app/static/js/2.6a68d53f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/food-app/static/js/2.6a68d53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e82a6b46a8c36adeb9",
+    "url": "/food-app/static/js/main.8288a836.chunk.js"
+  },
+  {
+    "revision": "7ebb87e7bbb0fd8d89d2",
+    "url": "/food-app/static/js/runtime-main.07c31e0d.js"
+  }
+]);
